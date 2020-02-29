7772eae0369e5c83481e5d855e935b64d3e294fc
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-02-29 20:48:44 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -69,7 +69,8 @@ async def approve_p_m(event):
                     await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
                     del borg.storage.PREV_REPLY_MESSAGE[chat.id]
                 approve(chat.id, reason)
-                await event.edit("`This bitch can msg you Now!`")
+                #await event.edit("`This bitch can msg you Now!`")
+                await event.edit("`Approved for pm!`")
                 await asyncio.sleep(3)
                 await event.delete()
 
