43673350c76297a87aa23588fbf94e9b103f3705
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-03-09 16:16:41 +0530

    Update giveawayhelper.py

--- stdplugins/giveawayhelper.py
+++ stdplugins/giveawayhelper.py
@@ -21,21 +21,37 @@ async def _(event):
   if event.reply_to_msg_id:
     previous_message = await event.get_reply_message()
     message = previous_message.message
-    raw_text = previous_message.raw_text
+    raw_text = previous_message.text
   error_count = 0
   for chat_id in channel.keys():
     try:
       await borg.send_message(chat_id, message)
       await borg.send_message(chat_id, raw_text)
-      await borg.send_message(logs_id, f"{message} sent at {channel[chat_id]} ({chat_id}) successfully.")
-
-    except:
-      try:
-        await borg.forward_messages(chat_id, previous_message)
-        await borg.send_message(logs_id, f"Message sent at {channel[chat_id]} ({chat_id}) successfully.")
-      except Exception as error:
-        await borg.send_message(logs_id, "Error! " + str(error))
-        error_count+=1
+    except Exception as error:
+      await borg.send_message(logs_id, f"Error in sending at {channel[chat_id]} ({chat_id}).")
+      await borg.send_message(logs_id, "Error! " + str(error))
+      error_count+=1
   await borg.send_message(logs_id, f"{error_count} Errors")
+  
+  
+@borg.on(admin_cmd("forward ?(.*)"))
+async def _(event):
+  if event.fwd_from:
+    return
+  input_str = event.pattern_match.group(1)
+  if event.reply_to_msg_id:
+    previous_message = await event.get_reply_message()
+    message = previous_message.message
+    raw_text = previous_message.raw_text
+  error_count = 0
+  for chat_id in channel.keys():
+    try:
+      await borg.forward_messages(chat_id, previous_message)
+    except Exception as error:
+      await borg.send_message(logs_id, f"Error in sending at {channel[chat_id]} ({chat_id}).")
+      await borg.send_message(logs_id, "Error! " + str(error))
+      error_count+=1
+  await borg.send_message(logs_id, f"{error_count} Errors")
+
 
 #client.send_message(chat_ids, message)
