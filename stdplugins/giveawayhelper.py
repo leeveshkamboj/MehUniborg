31450970d0c26a65674617bf3ad12de41fd76ec1
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-03-09 21:49:41 +0530

    Update giveawayhelper.py

--- stdplugins/giveawayhelper.py
+++ stdplugins/giveawayhelper.py
@@ -13,7 +13,7 @@ from telethon.tl.types import (
     MessageMediaPhoto
 )
 
-channel = {-1001191913647 : "Trash", -1001469149410 : "WWMethLabs"}
+channel = {-1001191913647 : "Trash", -1001469149410 : "@WWMethLabs", 1302017626 : "@bulbagiveaways"}
 logs_id = -411442681
 
 
@@ -26,6 +26,8 @@ error_msg = "Error"
 async def _(event):
   if event.fwd_from:
     return
+  error_count = 0
+  sent_count = 0
   input_str = event.pattern_match.group(1)
   if event.reply_to_msg_id:
     previous_message = await event.get_reply_message()
@@ -42,16 +44,17 @@ async def _(event):
                                 ),
                                 force_document=False
                             )
+          sent_count += 1
         except Exception as error:
           await borg.send_message(logs_id, f"Error in sending at {channel[chat_id]} ({chat_id}).")
           await borg.send_message(logs_id, "Error! " + str(error))
-          error_count+=1
+          error_count += 1
     else:
       raw_text = previous_message.text
-      error_count = 0
       for chat_id in channel.keys():
         try:
           await borg.send_message(chat_id, raw_text)
+          sent_count += 1
         except Exception as error:
           await borg.send_message(logs_id, f"Error in sending at {channel[chat_id]} ({chat_id}).")
           await borg.send_message(logs_id, "Error! " + str(error))
@@ -61,9 +64,11 @@ async def _(event):
 
   
 @borg.on(admin_cmd("forward ?(.*)"))
-async def _(event):
+async def _(event): 
   if event.fwd_from:
     return
+  error_count = 0
+  sent_count = 0 
   input_str = event.pattern_match.group(1)
   if event.reply_to_msg_id:
     previous_message = await event.get_reply_message()
@@ -73,6 +78,7 @@ async def _(event):
   for chat_id in channel.keys():
     try:
       await borg.forward_messages(chat_id, previous_message)
+      sent_count += 1
     except Exception as error:
       await borg.send_message(logs_id, f"Error in sending at {channel[chat_id]} ({chat_id}).")
       await borg.send_message(logs_id, "Error! " + str(error))
