28e2f93c0c731787594e660c272c0411ca02f5b1
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-03-29 18:04:44 +0530

    Update ss.py

--- stdplugins/ss.py
+++ stdplugins/ss.py
@@ -5,7 +5,10 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    NO_OF_SCSS = int(event.text.split(" ", maxsplit=1)[1])
+    try:
+        NO_OF_SCSS = int(event.text.split(" ", maxsplit=1)[1])
+    except:
+        NO_OF_SCSS = 1
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
