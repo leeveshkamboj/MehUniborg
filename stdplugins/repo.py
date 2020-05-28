638e155c7f16645ba137ab1d4534bf670ab80c60
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-05-28 15:04:08 +0530

    Update repo.py

--- stdplugins/repo.py
+++ stdplugins/repo.py
@@ -3,7 +3,7 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd("repo"))
 async def handler(event):
-    await event.edit("[Click here](https://github.com/Dc5000/MehUniBorg) to open this lit repo!")
+    await event.edit("[Click here](https://github.com/IG-Ricky/MehUniborg) to open this lit repo!")
     
 @borg.on(admin_cmd("mrepo"))
 async def handler(event):
