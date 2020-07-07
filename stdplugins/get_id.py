5df0503916c2f5bc8104a96ce5034f97ebe83a3b
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-07-07 12:56:05 +0530

    Update get_id.py

--- stdplugins/get_id.py
+++ stdplugins/get_id.py
@@ -29,10 +29,6 @@ async def _(event):
     if event.reply_to_msg_id:
         chat = await event.get_input_chat()
         r_msg = await event.get_reply_message()
-        if r_msg.media:
-            bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
-        else:
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nMsg ID {}".format(str(event.chat_id), str(r_msg.from_id), str(event.reply_to_msg_id) ))
+        await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nMsg ID {}".format(str(event.chat_id), str(r_msg.from_id), str(event.reply_to_msg_id) ))
     else:
         await event.edit("Current Chat ID: `{}`".format(str(event.chat_id)))
