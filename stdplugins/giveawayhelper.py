3fa0e3e5811154d6e8d39a9ac0b3388fc0625e03
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-07-27 12:02:08 +0530

    Update giveawayhelper.py

--- stdplugins/giveawayhelper.py
+++ stdplugins/giveawayhelper.py
@@ -61,7 +61,7 @@ async def _(event):
   channels = get_all_channels()
   error_count = 0
   sent_count = 0
-  await event.edit("Sending...")
+  await event.edit("Sending....")
   if event.reply_to_msg_id:
     previous_message = await event.get_reply_message()
     if previous_message.sticker or previous_message.poll:
